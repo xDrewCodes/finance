ef9eb43b56bb7d761f26c4e5ae02fca767c5227d
Drew Allred <drew.e.allred@gmail.com>
2026-08-19 18:35:18 -0400

    conditional render on sign in

--- src/App.jsx
+++ src/App.jsx
@@ -18,46 +18,49 @@ function App() {
     token: linkToken,
     onSuccess: (public_token) => {
       console.log("PUBLIC TOKEN:", public_token);
+      setSignedIn(true);
     }
   });
 
   return (
     <>
-      <header>
-        <div className="pfp"><div></div>Drew</div>
-        <div className="settings">Settings</div>
-      </header>
 
-      <section className="summary">
-        <div className="summary_filter">All Acounts</div>
-        <div className="total_bal"><span className="summary_title">Total Balance</span><br></br> $5,401.94</div>
-      </section>
-
-      <section className="bal_breakdown">
-        <div className="account_breakdown">Account Breakdown</div>
-        <div className="manage_money">Manage Money</div>
-      </section>
-
-
-      <h2 className="month_pl_legend">Month Recap</h2>
-      <section className="month_pl">
-        <div className="pl_title">
-          <span className="pl_bal">+1,429.55</span>
-        </div>
-        <div className="pl_chart">
-          <div className="pl_chart_block"></div>
-          <div className="pl_chart_block"></div>
-          <div className="pl_chart_block"></div>
-          <div className="pl_chart_block" style={{ background: "#193441" }}></div>
-          <div className="pl_chart_block"></div>
-          <div className="pl_chart_block"></div>
-          <div className="pl_chart_block"></div>
-        </div>
-      </section>
 
       {
         signedIn ?
-          ""
+          <>
+            <header>
+              <div className="pfp"><div></div>Drew</div>
+              <div className="settings">Settings</div>
+            </header>
+
+            <section className="summary">
+              <div className="summary_filter">All Acounts</div>
+              <div className="total_bal"><span className="summary_title">Total Balance</span><br></br> $5,401.94</div>
+            </section>
+
+            <section className="bal_breakdown">
+              <div className="account_breakdown">Account Breakdown</div>
+              <div className="manage_money">Manage Money</div>
+            </section>
+
+
+            <h2 className="month_pl_legend">Month Recap</h2>
+            <section className="month_pl">
+              <div className="pl_title">
+                <span className="pl_bal">+1,429.55</span>
+              </div>
+              <div className="pl_chart">
+                <div className="pl_chart_block"></div>
+                <div className="pl_chart_block"></div>
+                <div className="pl_chart_block"></div>
+                <div className="pl_chart_block" style={{ background: "#193441" }}></div>
+                <div className="pl_chart_block"></div>
+                <div className="pl_chart_block"></div>
+                <div className="pl_chart_block"></div>
+              </div>
+            </section>
+          </>
           :
           <button onClick={() => open()} disabled={!ready || !linkToken}>
             Connect Bank
