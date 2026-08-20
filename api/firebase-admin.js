f4b85cd672a03bb28457837d9a261f14bcd8354f
Drew Allred <drew.e.allred@gmail.com>
2026-08-20 19:40:26 -0400

    admin testing

--- api/firebase-admin.js
+++ api/firebase-admin.js
@@ -2,11 +2,16 @@ import admin from "firebase-admin";
 
 if (!admin.apps.length) {
 
-    if (
-        !process.env.FIREBASE_PROJECT_ID ||
-        !process.env.FIREBASE_CLIENT_EMAIL ||
-        !process.env.FIREBASE_PRIVATE_KEY
-    ) {
+    const projectId = process.env.FIREBASE_PROJECT_ID;
+    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+    console.log("Firebase Admin config:");
+    console.log("PROJECT_ID:", !!projectId);
+    console.log("CLIENT_EMAIL:", !!clientEmail);
+    console.log("PRIVATE_KEY:", !!privateKey);
+
+    if (!projectId || !clientEmail || !privateKey) {
         throw new Error(
             "Missing Firebase Admin environment variables"
         );
@@ -14,14 +19,9 @@ if (!admin.apps.length) {
 
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-
-            clientEmail:
-                process.env.FIREBASE_CLIENT_EMAIL,
-
-            privateKey:
-                process.env.FIREBASE_PRIVATE_KEY
-                    .replace(/\\n/g, "\n")
+            projectId,
+            clientEmail,
+            privateKey: privateKey.replace(/\\n/g, "\n")
         })
     });
 }
