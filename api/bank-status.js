87f2dfaf40f4fa422eff7608d829efe5f2dc32da
Drew Allred <drew.e.allred@gmail.com>
2026-08-20 19:52:36 -0400

    highkey test

--- api/bank-status.js
+++ api/bank-status.js
@@ -1,50 +1,8 @@
-import { auth, db } from "./firebase-admin.js";
-
 export default async function handler(req, res) {
+    console.log("BANK STATUS FUNCTION RAN");
 
-    try {
-
-        const authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            return res.status(401).json({
-                error: "No authorization header"
-            });
-        }
-
-        const idToken = authHeader.split("Bearer ")[1];
-
-        if (!idToken) {
-            return res.status(401).json({
-                error: "No Firebase ID token"
-            });
-        }
-
-        // Verify Firebase user
-        const decodedToken = await auth.verifyIdToken(idToken);
-
-        const uid = decodedToken.uid;
-
-        console.log("Firebase UID:", uid);
-
-        // Find Plaid items belonging to this user
-        const snapshot = await db
-            .collection("plaidItems")
-            .where("userId", "==", uid)
-            .get();
-
-        console.log("Plaid items found:", snapshot.size);
-
-        return res.status(200).json({
-            hasBanks: snapshot.size > 0
-        });
-
-    } catch (error) {
-
-        console.error("BANK STATUS ERROR:", error);
-
-        return res.status(500).json({
-            error: error.message
-        });
-    }
+    return res.status(200).json({
+        success: true,
+        message: "bank-status is working"
+    });
 }
